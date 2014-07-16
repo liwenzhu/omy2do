8604855062546775df6048a1474edc2625a0f3fe
liwenzhu <lee.wenzhu@gmail.com>
2014-07-16 22:26:06 +0800

    ready to release beta version.

--- todos/server/publish.js
+++ todos/server/publish.js
@@ -4,7 +4,12 @@ Lists = new Meteor.Collection("lists");
 // Publish complete set of lists to all clients.
 Meteor.publish('lists', function () {
   var owner = !!this.userId ? this.userId : 'public';
-  return Lists.find({owner: owner});
+  var lists = Lists.find({owner: owner});
+  if (lists.count()===0 && owner === 'public') {
+  	initDatabase();
+  	lists = Lists.find({owner: owner});
+  }
+  return lists;
 });
 
 
@@ -23,3 +28,55 @@ Meteor.publish('todos', function (list_id) {
   return Todos.find({list_id: list_id});
 });
 
+function initDatabase() {
+	console.log('init database');
+	var data = [
+      {name: "Meteor Principles",
+       contents: [
+         ["Data on the Wire", "Simplicity", "Better UX", "Fun"],
+         ["One Language", "Simplicity", "Fun"],
+         ["Database Everywhere", "Simplicity"],
+         ["Latency Compensation", "Better UX"],
+         ["Full Stack Reactivity", "Better UX", "Fun"],
+         ["Embrace the Ecosystem", "Fun"],
+         ["Simplicity Equals Productivity", "Simplicity", "Fun"]
+       ]
+      },
+      {name: "Languages",
+       contents: [
+         ["Lisp", "GC"],
+         ["C", "Linked"],
+         ["C++", "Objects", "Linked"],
+         ["Python", "GC", "Objects"],
+         ["Ruby", "GC", "Objects"],
+         ["JavaScript", "GC", "Objects"],
+         ["Scala", "GC", "Objects"],
+         ["Erlang", "GC"],
+         ["6502 Assembly", "Linked"]
+         ]
+      },
+      {name: "Favorite Scientists",
+       contents: [
+         ["Ada Lovelace", "Computer Science"],
+         ["Grace Hopper", "Computer Science"],
+         ["Marie Curie", "Physics", "Chemistry"],
+         ["Carl Friedrich Gauss", "Math", "Physics"],
+         ["Nikola Tesla", "Physics"],
+         ["Claude Shannon", "Math", "Computer Science"]
+       ]
+      }
+    ];
+
+    var timestamp = (new Date()).getTime();
+    for (var i = 0; i < data.length; i++) {
+      var list_id = Lists.insert({name: data[i].name, owner: 'public'});
+      for (var j = 0; j < data[i].contents.length; j++) {
+        var info = data[i].contents[j];
+        Todos.insert({list_id: list_id,
+                      text: info[0],
+                      timestamp: timestamp,
+                      tags: info.slice(1)});
+        timestamp += 1; // ensure unique timestamp.
+      }
+    }
+}
